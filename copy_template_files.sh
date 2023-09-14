4999a3ccddfe3ee8f4e0784955c55059c321825a
Patrick Straßer <p.strasser@dmesh.io>
2023-09-14 11:56:22 +0200

    add deployment code

---
+++ copy_template_files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Source directory
+source_dir="airbyte_connection_templates"
+
+# Destination directory
+destination_dir="octavia/sources/"
+
+airbyte_configuration_types=("sources" "destinations" "connections")
+
+string_list=("String1" "String2" "String3")
+
+for connection_type in "${airbyte_configuration_types[@]}"; do
+    for source_file in "$source_dir"/"$connection_type"/*.yaml; do
+        # Get the filename (without the path)
+        filename=$(basename "$source_file")
+
+        # Create the destination directory (if it doesn't exist)
+        mkdir -p "$destination_dir/$connection_type/$filename"
+
+        # Copy the source file to the destination with the desired name
+        cp "$source_file" "$destination_dir/$connection_type/$filename/configuration.yaml"
+    done
+done
+
+echo "Files copied successfully."
